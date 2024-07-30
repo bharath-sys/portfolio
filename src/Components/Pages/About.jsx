63a4d807b80f2c16be8573379585435aab703407
Bharath-kumar <bharath.kumar@insurancedekho.com>
2024-07-30 16:36:35 +0530

    dynamic project

--- src/Components/Pages/About.jsx
+++ src/Components/Pages/About.jsx
@@ -1,32 +1,9 @@
 import React, { useState } from 'react'
 import { Flex, Box, Heading, Text, Image } from "@chakra-ui/react";
 import ProjectModal from '../Modals/ProjectModal';
+import { useQuery } from "@tanstack/react-query";
+import { fetchDataByCondition } from "../../FireBase/api";
 
-const projects = [
-    {
-        title: "Talk-A-Tive (chat app)",
-        image: "https://firebasestorage.googleapis.com/v0/b/portfolio-668b7.appspot.com/o/meetme.png?alt=media&token=d8d96a11-f57c-47b6-965a-e6d854697cbb",
-        description: "Realtime chat using socket.io , jwt tokens authentication,",
-        demoLink : "https://chat-app-9xcv.onrender.com/login",
-        codeRepoLink : "https://github.com/bharath-sys/chat-app?tab=readme-ov-file"
-    },
-    {
-        title: "Food Deilvery App (backend)",
-        image: "https://firebasestorage.googleapis.com/v0/b/portfolio-668b7.appspot.com/o/meetme.png?alt=media&token=d8d96a11-f57c-47b6-965a-e6d854697cbb",
-        description: "documented API using postgresSql and node.js integrated with swagger ui",
-        demoLink : "https://food-app-backend-df1q.onrender.com",
-        codeRepoLink : "https://github.com/bharath-sys/food-app-backend"
-    },
-    // {
-    //     title: "Project 3",
-    //     description: "Description of project 1...",
-    // },
-    // {
-    //     title: "Project 4",
-    //     description: "Description of project 2...",
-    // }
-    // Add more projects as needed
-];
 
 const ProjectCard = ({ project }) => {
     const [isOpen, setIsOpen] = useState(false);
@@ -63,10 +40,14 @@ const ProjectCard = ({ project }) => {
 };
 
 const About = () => {
+    const { data, isLoading } = useQuery({
+        queryKey: ["fetchData",'project'],
+        queryFn: () => fetchDataByCondition({ page: 'about',collection:'Projects' }),
+      });
     return (
         // project section 
         <Flex overflowX="scroll" p="4" maxW={"90%"}>
-            {projects.map((project, index) => (
+            {data?.map((project, index) => (
                 <ProjectCard key={index} project={project} />
             ))}
         </Flex>
