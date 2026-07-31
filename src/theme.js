684513183f9ab6447594fe10b1f8d3b4d335a198
bharath-sys <kbk.bharath12345@gmail.com>
2026-07-31 23:12:13 +0530

    dark mode set

--- src/theme.js
+++ src/theme.js
@@ -12,7 +12,7 @@ export const ACID_INK = "var(--acid-ink)";
 
 const theme = extendTheme({
   config: {
-    initialColorMode: "light",
+    initialColorMode: "dark",
     useSystemColorMode: false,
   },
 
